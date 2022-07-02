a397ebaff6ec6c5d7a87e38ef7d7c5cfccdc8baa
bduncan4410 <43123556+bduncan4410@users.noreply.github.com>
2022-07-02 18:11:31 -0500

    Item Service API Create, Read, and Update operations were added for Item API

--- src/main/java/duncan/projects/ecomm/Model/Item.java
+++ src/main/java/duncan/projects/ecomm/Model/Item.java
@@ -19,24 +19,74 @@ public class Item
     private @NonNull double price;
     private @NonNull boolean isActive;
 
+    public Item(Long id, @NonNull String name, @NonNull String description, @NonNull String category, double price, boolean isActive)
+    {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.price = price;
+        this.isActive = isActive;
+    }
+
 
     public Long getId()
     {
         return id;
     }
 
-    public void setId(Long id)
+    @NonNull
+    public String getName()
     {
-        this.id = id;
+        return name;
     }
 
+    @NonNull
     public String getDescription()
     {
         return description;
     }
 
-    public void setDescription(String description)
+    @NonNull
+    public String getCategory()
     {
+        return category;
+    }
+
+    @NonNull
+    public double getPrice()
+    {
+        return price;
+    }
+
+    @NonNull
+    public boolean isActive()
+    {
+        return isActive;
+    }
+
+
+    public void setId(@NonNull Long id) {
+        this.id = id;
+    }
+
+    public void setName(@NonNull String name) {
+        this.name = name;
+    }
+
+    public void setDescription(@NonNull String description) {
         this.description = description;
+    }
+
+    public void setCategory(@NonNull String category) {
+        this.category = category;
+    }
+
+    public void setPrice(@NonNull double price) {
+        this.price = price;
+    }
+
+    public void setActive(@NonNull boolean active) {
+        isActive = active;
     }
 }
--- src/main/java/duncan/projects/ecomm/Service/ItemService.java
+++ src/main/java/duncan/projects/ecomm/Service/ItemService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -22,6 +23,25 @@ public class ItemService
     public List<Item> listItems()
     {
         return itemRepo.findAll();
+    }
+    public Item readItem(String itemName)
+    {
+        return itemRepo.findByItemName(itemName);
+    }
+    public Optional<Item> readItem(Integer itemID)
+    {
+        return itemRepo.findById(itemID);
+    }
+
+    public void updateItem(Integer itemID, Item newItem)
+    {
+        Item item = itemRepo.findById(itemID).get();
+        item.setName(newItem.getName());
+        item.setDescription(newItem.getDescription());
+        item.setActive(newItem.isActive());
+        item.setPrice(newItem.getPrice());
+        itemRepo.save(item);
+
     }
 
 
